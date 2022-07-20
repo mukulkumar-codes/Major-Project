2af82c25ad4e4770b6fb183e7fe30ad13d337477
Mukul Kumar <mukulkumar.codes@gmail.com>
2022-07-20 20:36:18 +0530

    directory and dart files are classified into different category to make it understandable

---
+++ android/app/src/main/kotlin/com/example/saci_chats/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.saci_chats
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/services/encryption_operations.dart
@@ -0,0 +1,43 @@
+import 'package:encrypt/encrypt.dart';
+
+import '../config/secret_data.dart';
+import '../config/text_collection.dart';
+import 'local_data_management.dart';
+
+class Secure {
+  static Key _key =
+  Key.fromBase64(DataManagement.getEnvData(EnvFileKey.encryptKey) ?? '');
+  static IV _iv = IV.fromLength(16);
+  static Encrypter _makeEncryption =
+  Encrypter(AES(_key, mode: AESMode.ctr, padding: null));
+
+  static String? encode(String? plainText) {
+    if (plainText == null) return null;
+    final Encrypted encrypted = _makeEncryption.encrypt(plainText, iv: _iv);
+    return encrypted.base64;
+  }
+
+  static String decode(String? encodedStringForm, {bool initialize = false}) {
+    try {
+      if (encodedStringForm == null) return '';
+
+      if (initialize) {
+        _key = Key.fromBase64(SecretData.encryptKey);
+        _iv = IV.fromLength(16);
+        _makeEncryption =
+            Encrypter(AES(_key, mode: AESMode.ctr, padding: null));
+      }
+
+      final String decrypted =
+      _makeEncryption.decrypt64(encodedStringForm, iv: _iv);
+
+      return decrypted;
+    } catch (e) {
+      if (e.toString().contains('NotInitializedError')) {
+        return decode(encodedStringForm, initialize: true);
+      }
+
+      return encodedStringForm ?? '';
+    }
+  }
+}
